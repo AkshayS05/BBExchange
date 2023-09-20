770d85c53049c29be4456c036fef9df0e8a9ac80
akshay <akshaysharma5432@gmail.com>
2023-09-20 19:42:12 -0400

    finish styles for quiz questions

--- wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
+++ wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7f95b7b7b048a1135bed');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7f065cf990ad8a090568');
