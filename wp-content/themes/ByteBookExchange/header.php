862c032a25064396157032d45de9a7fab8de5c2b
akshay <akshaysharma5432@gmail.com>
2023-09-21 00:20:02 -0400

    add blog icon

--- wp-content/themes/ByteBookExchange/header.php
+++ wp-content/themes/ByteBookExchange/header.php
@@ -4,6 +4,8 @@
   <head>
   <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/bbe_icon.jpg" type="image/x-icon">
+
     <!-- to get css styles 
     this lets wordpress be in control of our head section. it will load ehatever reqwuies using sytles from plugins
   -->
